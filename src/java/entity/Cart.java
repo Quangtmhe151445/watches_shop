67e35571f3a9980d9ea5be4d3e672e9515a8e1a5
Quangtmhe151445 <95202014+Quangtmhe151445@users.noreply.github.com>
2021-12-16 09:20:28 +0700

    Add files via upload

---
+++ src/java/entity/Cart.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Loki
+ */
+public class Cart {
+    private int productId;
+    private String name;
+    private int price;
+    private int quantity;
+    private String image;
+
+    public Cart() {
+    }
+
+    public Cart(int productId, String name, int price, int quantity, String image) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.image = image;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+        
+}
